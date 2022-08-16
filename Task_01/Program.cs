a7e2dfcfbcd24fcb9844fa630968fb1d3e714fbf
Artem <rrampage1991@gmail.com>
2022-08-16 22:00:12 +0300

    Task_01

---
+++ Task_01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным.
+
+Console.Write("Введите число обозначающее день недели[1;7] ");
+int userNumber = int.Parse(Console.ReadLine());
+if (userNumber <= 5)
+{
+    Console.Write(+userNumber + " не является выходным ");
+}
+if (userNumber > 5 && userNumber < 8)
+{
+    Console.Write(+userNumber + " Является выходным ");
+}
